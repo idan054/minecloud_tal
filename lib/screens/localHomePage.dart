83991d10ab53edc2e952957e8f707d329d49884f
Idan054 <idanbit80@gmail.com>
2022-07-10 19:28:46 +0300

    V7.0 localHomePage.dart BETA + BottomNavigator
    V6.1 localHomePage.dart BETA (unFinished)
    + worldCardWs.dart BETA based localHomePage.dart
    
    V5 resetPass_page.dart Added
    [ ] Add textStyle design while typing to darkTxtField()
    [ ] Add hide/ show eye icon on password field
    
    V4 login_page.dart Ui DONE
    
    V3 Great login_page.dart BETA (unFinished)
    
    V2: Lib STRUCTURE Updated
    + empty files added to screens & widgets folders (structure only)
    
    V1: Counter App & Colors.dart
    + colors.dart has been added from the XD Ui Design

--- lib/screens/localHomePage.dart
+++ lib/screens/localHomePage.dart
@@ -10,6 +10,10 @@ class LocalHomePage extends StatefulWidget {
 }
 
 class _LocalHomePageState extends State<LocalHomePage> {
+  int _selectedIndex = 0;
+
+  // final PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -44,8 +48,7 @@ class _LocalHomePageState extends State<LocalHomePage> {
             child: ListView.builder(
               itemCount: 3,
               itemBuilder: (context, index) {
-                return
-                  Card(
+                return Card(
                   margin:
                       const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
                   color: kTapBorderAssets,
@@ -75,27 +78,116 @@ class _LocalHomePageState extends State<LocalHomePage> {
                       style: poppinsRegular()
                           .copyWith(fontSize: 10, color: kDetailedWhite60),
                     ),
-                    trailing:
-                            Column(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: const BorderRadius.all(Radius.circular(4)),
-                                  child: Container(
-                                    color: kTapBorderAssets,
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 6, vertical: 4),
-                                    child: Text('SYNCED',
-                                        style: poppinsRegular()
-                                            .copyWith(fontSize: 10, color: kDetailedWhite60,
-                                        )),
+                    trailing: Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(4)),
+                          child: Container(
+                            color: kTapBorderAssets,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 6, vertical: 4),
+                            child: Text('SYNCED',
+                                style: poppinsRegular().copyWith(
+                                  fontSize: 10,
+                                  color: kDetailedWhite60,
+                                )),
+                          ),
+                        ),
+                      ],
                     ),
-                                ),
-                              ],
-                            ),
                   ),
                 );
               },
             ),
+          ),
+          floatingActionButton: Container(
+            margin: const EdgeInsets.only(bottom: 5),
+            alignment: Alignment.bottomCenter,
+            child: FloatingActionButton(
+              mini: true,
+              onPressed: () {},
+              child: Container(
+                width: 60,
+                height: 60,
+                child: const Icon(Icons.sync_outlined),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: positiveBlueButtonGradient
+                ),
+              ),
+            ),
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: kProgressBar,
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: kImportantWhite80,
+            unselectedItemColor: kDetailedWhite60,
+            selectedLabelStyle:
+                poppinsRegular().copyWith(color: kImportantWhite80),
+            unselectedLabelStyle:
+                poppinsRegular().copyWith(color: kDetailedWhite60),
+            currentIndex: _selectedIndex,
+            onTap: (value) {
+              // onTap Bottom icon
+              setState(() => _selectedIndex = value);
+              print('_selectedIndex $_selectedIndex');
+              // _pageController.jumpToPage(value);
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: _selectedIndex == 0
+                      ? Container(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 22, vertical: 4),
+                          color: kTapBorderAssets,
+                          child: Icon(
+                            Icons.folder_rounded,
+                            color: kDetailedWhite60,
+                            size: 20,
+                          ))
+                      : Container(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 22, vertical: 4),
+                          color: kEmptyColor,
+                          child: Icon(
+                            Icons.folder_open_rounded,
+                            color: kDetailedWhite60,
+                            size: 20,
+                          )),
+                ),
+                label: 'Local',
+              ),
+              BottomNavigationBarItem(
+                icon: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: _selectedIndex == 1
+                      ? Container(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 22, vertical: 4),
+                          color: kTapBorderAssets,
+                          child: Icon(
+                            Icons.cloud_rounded,
+                            color: kDetailedWhite60,
+                            size: 20,
+                          ))
+                      : Container(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 22, vertical: 4),
+                          color: kEmptyColor,
+                          child: Icon(
+                            Icons.cloud_outlined,
+                            color: kDetailedWhite60,
+                            size: 20,
+                          )),
+                ),
+                label: 'Cloud',
+              ),
+            ],
           ),
         ));
   }
